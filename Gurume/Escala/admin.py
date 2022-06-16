f9605b16ee0b4f5508bab13c0a65f80b79b52dc8
Bruno Ribeiro <brunofernandes.job@gmail.com>
2022-06-16 12:26:42 -0300

    Foi feito a criação das tabelas por restaurante e seus devidos usuarios

--- Gurume/Escala/admin.py
+++ Gurume/Escala/admin.py
@@ -1,18 +1,62 @@
 from django.contrib import admin
-from Escala.models import EscalaBotafogo, Atividade, Ipanema
+from Escala.models import BotafogoAtividade, IpanemaAtividade, FMALLAtividade, RDBAtividade, RioSulAtividade, TijucaAtividade, BotafogoEscala, IpanemaEscala, FMALLEscala, RDBEscala, RioSulEscala, TijucaEscala
 
-@admin.register(EscalaBotafogo)
+@admin.register(BotafogoEscala)
 class FuncionarioAdmin(admin.ModelAdmin):
     list_display = ['funcionario', 'dia']
     list_filter = ['funcionario', 'dia']
 
-@admin.register(Ipanema)
+@admin.register(IpanemaEscala)
 class FuncionarioAdmin(admin.ModelAdmin):
     list_display = ['funcionario', 'dia']
     list_filter = ['funcionario', 'dia']
 
-@admin.register(Atividade)
+@admin.register(FMALLEscala)
+class FuncionarioAdmin(admin.ModelAdmin):
+    list_display = ['funcionario', 'dia']
+    list_filter = ['funcionario', 'dia']    
+
+@admin.register(RDBEscala)
+class FuncionarioAdmin(admin.ModelAdmin):
+    list_display = ['funcionario', 'dia']
+    list_filter = ['funcionario', 'dia']
+
+@admin.register(RioSulEscala)
+class FuncionarioAdmin(admin.ModelAdmin):
+    list_display = ['funcionario', 'dia']
+    list_filter = ['funcionario', 'dia']
+
+@admin.register(TijucaEscala)
+class FuncionarioAdmin(admin.ModelAdmin):
+    list_display = ['funcionario', 'dia']
+    list_filter = ['funcionario', 'dia']
+
+@admin.register(BotafogoAtividade)
 class AtividadeAdmin(admin.ModelAdmin):
     list_display = ['hora', 'atividade', 'praca']
     list_filter = ['hora', 'atividade']
 
+@admin.register(IpanemaAtividade)
+class AtividadeAdmin(admin.ModelAdmin):
+    list_display = ['hora', 'atividade', 'praca']
+    list_filter = ['hora', 'atividade']
+
+@admin.register(FMALLAtividade)
+class AtividadeAdmin(admin.ModelAdmin):
+    list_display = ['hora', 'atividade', 'praca']
+    list_filter = ['hora', 'atividade']
+
+@admin.register(RDBAtividade)
+class AtividadeAdmin(admin.ModelAdmin):
+    list_display = ['hora', 'atividade', 'praca']
+    list_filter = ['hora', 'atividade']
+
+@admin.register(RioSulAtividade)
+class AtividadeAdmin(admin.ModelAdmin):
+    list_display = ['hora', 'atividade', 'praca']
+    list_filter = ['hora', 'atividade']
+
+@admin.register(TijucaAtividade)
+class AtividadeAdmin(admin.ModelAdmin):
+    list_display = ['hora', 'atividade', 'praca']
+    list_filter = ['hora', 'atividade']
--- Gurume/Escala/models.py
+++ Gurume/Escala/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
-# Create your models here.
-class Atividade(models.Model):
+# Classes de Atividades por Restaurante
+class BotafogoAtividade(models.Model):
     atividade_choice = (
         ('Fol','Folga'),('Pro','Produção'),('Fin','Finalização'),('Sla','Slash'),('QA','QA'),('Inv','Inventário'),('Fec','Fechamento'),('Alm','Almoço'),('Jan','Janta')
         )
@@ -26,22 +26,183 @@ class Atividade(models.Model):
     def __str__(self):
         return str(self.get_atividade_display())
 
-class EscalaBotafogo(models.Model):
+class IpanemaAtividade(models.Model):
+    atividade_choice = (
+        ('Fol','Folga'),('Pro','Produção'),('Fin','Finalização'),('Sla','Slash'),('QA','QA'),('Inv','Inventário'),('Fec','Fechamento'),('Alm','Almoço'),('Jan','Janta')
+        )
+
+    hora_choice = (
+        ('7','7 Hrs'),('8','8 Hrs'),('9','9 Hrs'),('10','10 Hrs'),('11','11 Hrs'),('12','12 Hrs'),('13','13 Hrs'),('14','14 Hrs'),
+        ('15','15 Hrs'),('16','16 Hrs'),('17','17 Hrs'),('18','18 Hrs'),('19','19 Hrs'),('20','20 Hrs'),('21','21 Hrs'),('22','22 Hrs'),
+        ('23','23 Hrs'),('00','00 Hrs'),('01','01 Hrs')
+        )
+
+    praca_choice = (
+        ('N','Nenhuma'),('C','Combinados'),('ENT','Entradas'),('ES','Entradas Sushibar'),
+        ('Rol','Rolls'),('RQ','Rolls Quentes'),('RF','Rolls Frios'),('BDE','Brisas Duplas Entradas'),
+        ('Fri','Fritadeira'),('FOG','Fogão'),('Sob','Sobremesas'),('Sus','Sushi Brisas'),('Lim','Limpeza'),
+        ('Pro','Produção')
+        )
+
+    hora = models.CharField(max_length=250, choices=hora_choice, null=False, blank=False)
+    atividade = models.CharField(max_length=250, choices=atividade_choice, null=False, blank=False)
+    praca = models.CharField(max_length=250, choices=praca_choice, default='Nenhuma')
+
+    def __str__(self):
+        return str(self.get_atividade_display())
+
+class FMALLAtividade(models.Model):
+    atividade_choice = (
+        ('Fol','Folga'),('Pro','Produção'),('Fin','Finalização'),('Sla','Slash'),('QA','QA'),('Inv','Inventário'),('Fec','Fechamento'),('Alm','Almoço'),('Jan','Janta')
+        )
+
+    hora_choice = (
+        ('7','7 Hrs'),('8','8 Hrs'),('9','9 Hrs'),('10','10 Hrs'),('11','11 Hrs'),('12','12 Hrs'),('13','13 Hrs'),('14','14 Hrs'),
+        ('15','15 Hrs'),('16','16 Hrs'),('17','17 Hrs'),('18','18 Hrs'),('19','19 Hrs'),('20','20 Hrs'),('21','21 Hrs'),('22','22 Hrs'),
+        ('23','23 Hrs'),('00','00 Hrs'),('01','01 Hrs')
+        )
+
+    praca_choice = (
+        ('N','Nenhuma'),('C','Combinados'),('ENT','Entradas'),('ES','Entradas Sushibar'),
+        ('Rol','Rolls'),('RQ','Rolls Quentes'),('RF','Rolls Frios'),('BDE','Brisas Duplas Entradas'),
+        ('Fri','Fritadeira'),('FOG','Fogão'),('Sob','Sobremesas'),('Sus','Sushi Brisas'),('Lim','Limpeza'),
+        ('Pro','Produção')
+        )
+
+    hora = models.CharField(max_length=250, choices=hora_choice, null=False, blank=False)
+    atividade = models.CharField(max_length=250, choices=atividade_choice, null=False, blank=False)
+    praca = models.CharField(max_length=250, choices=praca_choice, default='Nenhuma')
+
+    def __str__(self):
+        return str(self.get_atividade_display())    
+
+class RDBAtividade(models.Model):
+    atividade_choice = (
+        ('Fol','Folga'),('Pro','Produção'),('Fin','Finalização'),('Sla','Slash'),('QA','QA'),('Inv','Inventário'),('Fec','Fechamento'),('Alm','Almoço'),('Jan','Janta')
+        )
+
+    hora_choice = (
+        ('7','7 Hrs'),('8','8 Hrs'),('9','9 Hrs'),('10','10 Hrs'),('11','11 Hrs'),('12','12 Hrs'),('13','13 Hrs'),('14','14 Hrs'),
+        ('15','15 Hrs'),('16','16 Hrs'),('17','17 Hrs'),('18','18 Hrs'),('19','19 Hrs'),('20','20 Hrs'),('21','21 Hrs'),('22','22 Hrs'),
+        ('23','23 Hrs'),('00','00 Hrs'),('01','01 Hrs')
+        )
+
+    praca_choice = (
+        ('N','Nenhuma'),('C','Combinados'),('ENT','Entradas'),('ES','Entradas Sushibar'),
+        ('Rol','Rolls'),('RQ','Rolls Quentes'),('RF','Rolls Frios'),('BDE','Brisas Duplas Entradas'),
+        ('Fri','Fritadeira'),('FOG','Fogão'),('Sob','Sobremesas'),('Sus','Sushi Brisas'),('Lim','Limpeza'),
+        ('Pro','Produção')
+        )
+
+    hora = models.CharField(max_length=250, choices=hora_choice, null=False, blank=False)
+    atividade = models.CharField(max_length=250, choices=atividade_choice, null=False, blank=False)
+    praca = models.CharField(max_length=250, choices=praca_choice, default='Nenhuma')
+
+    def __str__(self):
+        return str(self.get_atividade_display())
+
+class RioSulAtividade(models.Model):
+    atividade_choice = (
+        ('Fol','Folga'),('Pro','Produção'),('Fin','Finalização'),('Sla','Slash'),('QA','QA'),('Inv','Inventário'),('Fec','Fechamento'),('Alm','Almoço'),('Jan','Janta')
+        )
+
+    hora_choice = (
+        ('7','7 Hrs'),('8','8 Hrs'),('9','9 Hrs'),('10','10 Hrs'),('11','11 Hrs'),('12','12 Hrs'),('13','13 Hrs'),('14','14 Hrs'),
+        ('15','15 Hrs'),('16','16 Hrs'),('17','17 Hrs'),('18','18 Hrs'),('19','19 Hrs'),('20','20 Hrs'),('21','21 Hrs'),('22','22 Hrs'),
+        ('23','23 Hrs'),('00','00 Hrs'),('01','01 Hrs')
+        )
+
+    praca_choice = (
+        ('N','Nenhuma'),('C','Combinados'),('ENT','Entradas'),('ES','Entradas Sushibar'),
+        ('Rol','Rolls'),('RQ','Rolls Quentes'),('RF','Rolls Frios'),('BDE','Brisas Duplas Entradas'),
+        ('Fri','Fritadeira'),('FOG','Fogão'),('Sob','Sobremesas'),('Sus','Sushi Brisas'),('Lim','Limpeza'),
+        ('Pro','Produção')
+        )
+
+    hora = models.CharField(max_length=250, choices=hora_choice, null=False, blank=False)
+    atividade = models.CharField(max_length=250, choices=atividade_choice, null=False, blank=False)
+    praca = models.CharField(max_length=250, choices=praca_choice, default='Nenhuma')
+
+    def __str__(self):
+        return str(self.get_atividade_display())
+
+class TijucaAtividade(models.Model):
+    atividade_choice = (
+        ('Fol','Folga'),('Pro','Produção'),('Fin','Finalização'),('Sla','Slash'),('QA','QA'),('Inv','Inventário'),('Fec','Fechamento'),('Alm','Almoço'),('Jan','Janta')
+        )
+
+    hora_choice = (
+        ('7','7 Hrs'),('8','8 Hrs'),('9','9 Hrs'),('10','10 Hrs'),('11','11 Hrs'),('12','12 Hrs'),('13','13 Hrs'),('14','14 Hrs'),
+        ('15','15 Hrs'),('16','16 Hrs'),('17','17 Hrs'),('18','18 Hrs'),('19','19 Hrs'),('20','20 Hrs'),('21','21 Hrs'),('22','22 Hrs'),
+        ('23','23 Hrs'),('00','00 Hrs'),('01','01 Hrs')
+        )
+
+    praca_choice = (
+        ('N','Nenhuma'),('C','Combinados'),('ENT','Entradas'),('ES','Entradas Sushibar'),
+        ('Rol','Rolls'),('RQ','Rolls Quentes'),('RF','Rolls Frios'),('BDE','Brisas Duplas Entradas'),
+        ('Fri','Fritadeira'),('FOG','Fogão'),('Sob','Sobremesas'),('Sus','Sushi Brisas'),('Lim','Limpeza'),
+        ('Pro','Produção')
+        )
+
+    hora = models.CharField(max_length=250, choices=hora_choice, null=False, blank=False)
+    atividade = models.CharField(max_length=250, choices=atividade_choice, null=False, blank=False)
+    praca = models.CharField(max_length=250, choices=praca_choice, default='Nenhuma')
+
+    def __str__(self):
+        return str(self.get_atividade_display())    
+
+# Classes de Escala por restaurante
+class BotafogoEscala(models.Model):
     dia_choice = (
         ('Dom','Domingo'),('Seg','Segunda'),('Ter','Terça'),('Qua','Quarta'),('Qui','Quinta'),('Sex','Sexta'),('Sab','Sábado')
         )
 
     funcionario = models.CharField(max_length=100, null=False, blank=False)
     dia = models.CharField(max_length=250, choices=dia_choice, null=False, blank=False)
-    atividade = models.ManyToManyField(Atividade)
-    
+    atividade = models.ManyToManyField(BotafogoAtividade)
 
-class Ipanema(models.Model):
+class IpanemaEscala(models.Model):
     dia_choice = (
         ('Dom','Domingo'),('Seg','Segunda'),('Ter','Terça'),('Qua','Quarta'),('Qui','Quinta'),('Sex','Sexta'),('Sab','Sábado')
         )
 
     funcionario = models.CharField(max_length=100, null=False, blank=False)
     dia = models.CharField(max_length=250, choices=dia_choice, null=False, blank=False)
-    atividade = models.ManyToManyField(Atividade)
-    
+    atividade = models.ManyToManyField(IpanemaAtividade)
+
+
+class RDBEscala(models.Model):
+    dia_choice = (
+        ('Dom','Domingo'),('Seg','Segunda'),('Ter','Terça'),('Qua','Quarta'),('Qui','Quinta'),('Sex','Sexta'),('Sab','Sábado')
+        )
+
+    funcionario = models.CharField(max_length=100, null=False, blank=False)
+    dia = models.CharField(max_length=250, choices=dia_choice, null=False, blank=False)
+    atividade = models.ManyToManyField(RDBAtividade)
+
+class FMALLEscala(models.Model):
+    dia_choice = (
+        ('Dom','Domingo'),('Seg','Segunda'),('Ter','Terça'),('Qua','Quarta'),('Qui','Quinta'),('Sex','Sexta'),('Sab','Sábado')
+        )
+
+    funcionario = models.CharField(max_length=100, null=False, blank=False)
+    dia = models.CharField(max_length=250, choices=dia_choice, null=False, blank=False)
+    atividade = models.ManyToManyField(FMALLAtividade)
+
+class TijucaEscala(models.Model):
+    dia_choice = (
+        ('Dom','Domingo'),('Seg','Segunda'),('Ter','Terça'),('Qua','Quarta'),('Qui','Quinta'),('Sex','Sexta'),('Sab','Sábado')
+        )
+
+    funcionario = models.CharField(max_length=100, null=False, blank=False)
+    dia = models.CharField(max_length=250, choices=dia_choice, null=False, blank=False)
+    atividade = models.ManyToManyField(TijucaAtividade)
+
+class RioSulEscala(models.Model):
+    dia_choice = (
+        ('Dom','Domingo'),('Seg','Segunda'),('Ter','Terça'),('Qua','Quarta'),('Qui','Quinta'),('Sex','Sexta'),('Sab','Sábado')
+        )
+
+    funcionario = models.CharField(max_length=100, null=False, blank=False)
+    dia = models.CharField(max_length=250, choices=dia_choice, null=False, blank=False)
+    atividade = models.ManyToManyField(RioSulAtividade)
