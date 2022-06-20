10cb357e8daa0931cc4c102bd4555ee95cc41dfd
Bruno Ribeiro <brunofernandes.job@gmail.com>
2022-06-20 13:40:10 -0300

    ok

--- Gurume/Gurume/urls.py
+++ Gurume/Gurume/urls.py
@@ -19,6 +19,6 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('https://gurume-escalas.netlify.app/', admin.site.urls),
+    path('admin/', admin.site.urls),
 ]
 static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
